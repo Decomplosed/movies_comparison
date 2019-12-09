aef0d95157d9df123c3d4b29f57971287e1548b7
Bart Frackiewicz <decomplosed@gmail.com>
2019-12-09 20:08:42 +0100

    refactored the fetchData so we won't make call to API every time user puts some characters inside input

--- index.js
+++ index.js
@@ -9,7 +9,16 @@ const fetchData = async (searchTerm) => {
   console.log(res.data)
 }
 
+let timeoutId
+
+const onInput = e => {
+  if (timeoutId) {
+    clearTimeout(timeoutId)
+  }
+  timeoutId = setTimeout(() => {
+    fetchData(e.target.value)
+  }, 500)
+}
+
 const input = document.querySelector('input')
-input.addEventListener('input', (e) => {
-  fetchData(e.target.value)
-})
+input.addEventListener('input', onInput)
