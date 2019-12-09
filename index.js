f35e74a3989b721d5bc4daec8169ac27b13ebb5a
Bart Frackiewicz <decomplosed@gmail.com>
2019-12-09 19:47:21 +0100

    created initial fetchData method

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('Hi there!');
+const fetchData = async () => {
+  const res = await axios.get('https://www.omdbapi.com/', {
+    params: {
+      apikey: 'c49531a8',
+      s: 'avengers'
+    }
+  })
+
+  console.log(res.data)
+}
+
+fetchData()
