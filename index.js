8d68300fdc03981c675e8dd803c4694b6b89925c
Bart Frackiewicz <decomplosed@gmail.com>
2019-12-10 22:49:11 +0100

    removed additional code just used for testing purposes

--- index.js
+++ index.js
@@ -17,14 +17,6 @@ createAutoComplete({
   root: document.querySelector('.autocomplete')
 })
 
-createAutoComplete({
-  root: document.querySelector('.autocomplete-two')
-})
-
-createAutoComplete({
-  root: document.querySelector('.autocomplete-three')
-})
-
 const onMovieSelect = async movie => {
   const res = await axios.get('https://www.omdbapi.com/', {
     params: {
